e2f5bf6681875056397741c4c905433fafc6b047
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-02 20:37:45 +0000

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //variables are used to store data
+        int age = 3; //we are initialising the variable
+        System.out.println(age);
+    }
+}
