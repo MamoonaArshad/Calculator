79b8f7d9498a809c109e14b1ea0e12f21ded78fd
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-02 21:01:09 +0000

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         //variables are used to store data
-        int age = 3; //we are initialising the variable
-        System.out.println(age);
+        int myAge = 3;
+        int herAge = myAge + 4;
+        int viewsCount = 123456789;
+        Date now  = new Date ();
+        now.getTime();
+
+
+        //we are initialising the variable
+        System.out.println(herAge);
     }
 }
+
+//there are two type: primitive and reference;
+//primtive types are used to store sumple variables like numbers, characters and booleans.
+//Reference types on the other hand are used to store more complicated objects like date or mail messages
